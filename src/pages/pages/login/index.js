870c77608a1df2fb1899a88c4514286ee28a8160
펠리페 <filip9322@gmail.com>
2023-03-21 17:58:22 +0900

    :giraffe: 1st commit before coustomizing

---
+++ src/pages/pages/login/index.js
@@ -0,0 +1,155 @@
+// ** React Imports
+import { useState } from 'react'
+
+// ** Next Imports
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+// ** MUI Components
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Divider from '@mui/material/Divider'
+import Checkbox from '@mui/material/Checkbox'
+import TextField from '@mui/material/TextField'
+import InputLabel from '@mui/material/InputLabel'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import CardContent from '@mui/material/CardContent'
+import FormControl from '@mui/material/FormControl'
+import OutlinedInput from '@mui/material/OutlinedInput'
+import { styled, useTheme } from '@mui/material/styles'
+import MuiCard from '@mui/material/Card'
+import InputAdornment from '@mui/material/InputAdornment'
+import MuiFormControlLabel from '@mui/material/FormControlLabel'
+
+// ** Icons Imports
+import EyeOutline from 'mdi-material-ui/EyeOutline'
+import EyeOffOutline from 'mdi-material-ui/EyeOffOutline'
+
+// ** Configs
+import themeConfig from 'src/configs/themeConfig'
+
+// ** Layout Import
+import BlankLayout from 'src/@core/layouts/BlankLayout'
+
+// ** Demo Imports
+import FooterIllustrationsV1 from 'src/views/pages/auth/FooterIllustration'
+
+// ** Styled Components
+const Card = styled(MuiCard)(({ theme }) => ({
+  [theme.breakpoints.up('sm')]: { width: '28rem' }
+}))
+
+const LinkStyled = styled('a')(({ theme }) => ({
+  fontSize: '0.875rem',
+  textDecoration: 'none',
+  color: theme.palette.primary.main
+}))
+
+const FormControlLabel = styled(MuiFormControlLabel)(({ theme }) => ({
+  '& .MuiFormControlLabel-label': {
+    fontSize: '0.875rem',
+    color: theme.palette.text.secondary
+  }
+}))
+
+const LoginPage = () => {
+  // ** State
+  const [values, setValues] = useState({
+    password: '',
+    showPassword: false
+  })
+
+  // ** Hook
+  const theme = useTheme()
+  const router = useRouter()
+
+  const handleChange = prop => event => {
+    setValues({ ...values, [prop]: event.target.value })
+  }
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword })
+  }
+
+  const handleMouseDownPassword = event => {
+    event.preventDefault()
+  }
+
+  return (
+    <Box className='content-center'>
+      <Card sx={{ zIndex: 1 }}>
+        <CardContent sx={{ padding: theme => `${theme.spacing(12, 9, 7)} !important` }}>
+          <Box sx={{ mb: 8, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Typography
+              variant='h6'
+              sx={{
+                ml: 3,
+                lineHeight: 1,
+                fontWeight: 600,
+                textTransform: 'uppercase',
+                fontSize: '1.5rem !important'
+              }}
+            >
+              {themeConfig.templateName}
+            </Typography>
+          </Box>
+          <Box sx={{ mb: 6, display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+            <Typography variant='h5' sx={{ fontWeight: 600, marginBottom: 1.5  }}>
+              로그인
+            </Typography>
+            <img  src='/images/pages/hangil_logo.png'  style={{width: '100px'}} alt="한길 로고"/>
+            <Typography variant='body2'>IOT 기반의 실시간 음향신호기 모니터링 시스템</Typography>
+            <Typography variant='body2'>BLE 기반의 실시간 음성유도기 모니터링 시스템</Typography>
+          </Box>
+          <form noValidate autoComplete='off' onSubmit={e => e.preventDefault()}>
+            <TextField autoFocus fullWidth id='email' label='아이디' sx={{ marginBottom: 4 }} />
+            <FormControl fullWidth>
+              <InputLabel htmlFor='auth-login-password'>비밀번호</InputLabel>
+              <OutlinedInput
+                label='Password'
+                value={values.password}
+                id='auth-login-password'
+                onChange={handleChange('password')}
+                type={values.showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position='end'>
+                    <IconButton
+                      edge='end'
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      aria-label='toggle password visibility'
+                    >
+                      {values.showPassword ? <EyeOutline /> : <EyeOffOutline />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <Box
+              sx={{ mb: 4, display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'space-between' }}
+            >
+              <FormControlLabel control={<Checkbox />} label='로그인 상태 유지' />
+              <Link passHref href='/'>
+                <LinkStyled onClick={e => e.preventDefault()}>비밀번호 찾기</LinkStyled>
+              </Link>
+            </Box>
+            <Button
+              fullWidth
+              size='large'
+              variant='contained'
+              sx={{ marginBottom: 7 }}
+              onClick={() => router.push('/')}
+            >
+              로그인
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+      <FooterIllustrationsV1 />
+    </Box>
+  )
+}
+LoginPage.getLayout = page => <BlankLayout>{page}</BlankLayout>
+
+export default LoginPage
