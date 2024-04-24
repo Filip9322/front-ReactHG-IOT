033c111b7e1a9368e301cd78f4a4c4a6ef684c8f
펠리페 <filip9322@gmail.com>
2024-04-24 17:56:50 +0900

    :dragon_face: CREATE subfile to handle REact Component for the form when editing a device beside the MapView :real:

---
+++ src/pages/controller/FormEditSelectedDevice.js
@@ -0,0 +1,13 @@
+// ** Reacts Imports
+import { useState, useEffect, useRef } from 'react';
+import { Box, Button, Tooltip, FormGroup, FormControlLabel, Checkbox, 
+  Typography, TextField, Input, Snackbar }  from '@mui/material';
+
+const FormEditSelectedDevice = props => {
+
+  //const {} = props;
+
+  return(
+    
+  )
+}
